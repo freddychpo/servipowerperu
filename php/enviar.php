ea74862efe1945064cc4a4ad636bdf4320f29398
freddychpo <freddychpo@gmail.com>
2018-07-16 21:31:13 -0500

    Add files via upload

---
+++ php/enviar.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$destino = "freddychpo@gmail.com";
+	$nombre = $_POST["nombre"];
+	$email = $_POST["email"];
+	$telefono = $_POST["telefono"];
+	$mensaje = $_POST["mensaje"];
+	$contenido = "Nombre: " . nombre . "\nEmail: " . correo . "\nTeléfono: " . telefono . "\nMensaje: " . mensaje
+
+	
+
+ ?>
